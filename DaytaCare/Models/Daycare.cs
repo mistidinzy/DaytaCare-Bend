9b00d7117612a5a79abfcf60ffe2750901985af8
Ian Whitmore <ianwhitmor@gmail.com>
2021-11-02 09:23:07 -0500

    create model folder, add daycare model class, add required params

---
+++ DaytaCare/Models/Daycare.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DaytaCare.Models
+{
+    public class Daycare
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string StreetAddress { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [Column(TypeName = "money")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int LicenseNumber { get; set; }
+    }
+}
+
