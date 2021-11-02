8011cccc7a000eaf01a8e7b38db3c89838477901
Ian Whitmore <ianwhitmor@gmail.com>
2021-11-02 12:20:58 -0500

    create daycares controller

---
+++ DaytaCare/Controllers/DaycaresController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DaytaCare.Data;
+using DaytaCare.Models;
+
+namespace DaytaCare.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DaycaresController : ControllerBase
+    {
+        private readonly DaytaCareDbContext _context;
+
+        public DaycaresController(DaytaCareDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Daycares
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Daycare>>> GetDaycares()
+        {
+            return await _context.Daycares.ToListAsync();
+        }
+
+        // GET: api/Daycares/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Daycare>> GetDaycare(int id)
+        {
+            var daycare = await _context.Daycares.FindAsync(id);
+
+            if (daycare == null)
+            {
+                return NotFound();
+            }
+
+            return daycare;
+        }
+
+        // PUT: api/Daycares/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDaycare(int id, Daycare daycare)
+        {
+            if (id != daycare.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(daycare).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DaycareExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Daycares
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Daycare>> PostDaycare(Daycare daycare)
+        {
+            _context.Daycares.Add(daycare);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDaycare", new { id = daycare.Id }, daycare);
+        }
+
+        // DELETE: api/Daycares/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDaycare(int id)
+        {
+            var daycare = await _context.Daycares.FindAsync(id);
+            if (daycare == null)
+            {
+                return NotFound();
+            }
+
+            _context.Daycares.Remove(daycare);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DaycareExists(int id)
+        {
+            return _context.Daycares.Any(e => e.Id == id);
+        }
+    }
+}
