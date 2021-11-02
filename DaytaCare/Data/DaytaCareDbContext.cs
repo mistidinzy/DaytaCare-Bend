a0ba4c42646cdeed50f4067432de452c979c9768
ymclapp <ymclapp@yahoo.com>
2021-11-02 11:54:03 -0500

    initial identity setup

--- DaytaCare/Data/DaytaCareDbContext.cs
+++ DaytaCare/Data/DaytaCareDbContext.cs
@@ -1,12 +1,18 @@
-﻿using System;
+using System;
+using DaytaCare.Models.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DaytaCare.Data
 {
-    public class DaytaCareDbContext : DbContext
+    public class DaytaCareDbContext : IdentityDbContext<ApplicationUser>
     {
         public DaytaCareDbContext(DbContextOptions options) : base(options)
         {
         }
+    protected override void OnModelCreating ( ModelBuilder modelbuilder )
+    {
+      base.OnModelCreating(modelbuilder);
     }
+  }
 }
