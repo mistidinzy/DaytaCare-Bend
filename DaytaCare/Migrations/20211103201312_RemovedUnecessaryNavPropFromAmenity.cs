b5f526feffcb9bdab43ce315579f2ee74cda6e06
mistidinzy <mistidinzy@gmail.com>
2021-11-03 15:13:58 -0500

    added migration and db update

---
+++ DaytaCare/Migrations/20211103201312_RemovedUnecessaryNavPropFromAmenity.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DaytaCare.Migrations
+{
+    public partial class RemovedUnecessaryNavPropFromAmenity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DaycareAmenities_AmenityId",
+                table: "DaycareAmenities");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DaycareAmenities_AmenityId",
+                table: "DaycareAmenities",
+                column: "AmenityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DaycareAmenities_AmenityId",
+                table: "DaycareAmenities");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DaycareAmenities_AmenityId",
+                table: "DaycareAmenities",
+                column: "AmenityId",
+                unique: true);
+        }
+    }
+}
