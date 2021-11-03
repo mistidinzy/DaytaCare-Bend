098b639ff8e376cfd1b42f17763c2c7de5d72658
mistidinzy <mistidinzy@gmail.com>
2021-11-03 15:37:52 -0500

    fixing spelling again

--- DaytaCare/Controllers/DaycaresController.cs
+++ DaytaCare/Controllers/DaycaresController.cs
@@ -101,18 +101,18 @@ namespace DaytaCare.Controllers
 
         [HttpPost]
         [Route("{id}/Amenities/{amenityId}")]
-        public async Task<ActionResult<DaycareAmenity>> PostAmenity(int id, int amenityId)
+        public async Task<ActionResult<DaycareAmenity>> PostAmenity(int Id, int amenityId)
         {
-            await daycares.AddAmenity(id, amenityId);
+            await daycares.AddAmenity(Id, amenityId);
 
             return NoContent();
         }
 
         [HttpDelete]
         [Route("{id}/Amenities/{amenityId}")]
-        public async Task<ActionResult<DaycareAmenity>> DeleteAmenity(int id, int amenityId)
+        public async Task<ActionResult<DaycareAmenity>> DeleteAmenity(int Id, int amenityId)
         {
-            await daycares.DeleteAmenity(id, amenityId);
+            await daycares.DeleteAmenity(Id, amenityId);
 
             return NoContent();
         }
