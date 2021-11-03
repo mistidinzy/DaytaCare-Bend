58f1b5687d9ca181715283ec86592d23497e3214
Ian Whitmore <ianwhitmor@gmail.com>
2021-11-02 22:32:19 -0500

    add GetAll method to Daycare interface

--- DaytaCare/Services/IDaycareRepository.cs
+++ DaytaCare/Services/IDaycareRepository.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DaytaCare.Models;
+
 namespace DaytaCare.Services
 {
     public interface IDaycareRepository
     {
+        Task<List<Daycare>> GetAll();
     }
 }
