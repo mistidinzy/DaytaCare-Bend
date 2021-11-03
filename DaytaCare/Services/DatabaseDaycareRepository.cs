6713693ab088bb6c1ca72fe86941b0e4ee687fb9
Ian Whitmore <ianwhitmor@gmail.com>
2021-11-02 22:27:40 -0500

    take dependency on scoped DbContext

--- DaytaCare/Services/DatabaseDaycareRepository.cs
+++ DaytaCare/Services/DatabaseDaycareRepository.cs
@@ -1,7 +1,15 @@
 ﻿using System;
+using DaytaCare.Data;
+
 namespace DaytaCare.Services
 {
     public class DatabaseDaycareRepository : IDaycareRepository
     {
+        private readonly DaytaCareDbContext _context;
+
+        public DatabaseDaycareRepository(DaytaCareDbContext context)
+        {
+            _context = context;
+        }
     }
 }
