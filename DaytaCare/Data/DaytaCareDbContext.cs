3812cf4aa465fd8587b8149b9afe8b03a7cb26f7
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-11-04 14:59:35 -0500

    added migration for seeded data

--- DaytaCare/Data/DaytaCareDbContext.cs
+++ DaytaCare/Data/DaytaCareDbContext.cs
@@ -40,7 +40,21 @@ namespace DaytaCare.Data
                 new Daycare { Id = 3, Name = "Home Ties Child Care Center", StreetAddress = "405 Myrtle Ave", City = "Iowa City", State = "Iowa", Country = "United States", Phone = "(319) 341-0050", Email = "iowa4cshometies@yahoo.com", Price = 190, DaycareType = DaycareType.LicensedCenter, LicenseNumber = 3, Availability = true },
                 new Daycare { Id = 4, Name = "Alice's Rainbow Child Care Center", StreetAddress = "421 Melrose Ave", City = "Iowa City", State = "Iowa", Country = "United States", Phone = "(319) 354-1466", Email = "alicesrainbowchildcarecenters@gmail.com", Price = 225, DaycareType = DaycareType.InHomeA, LicenseNumber = 4, Availability = false },
                 new Daycare { Id = 5, Name = "KIDS INC.", StreetAddress = "1100 35th St", City = "Marion", State = "Iowa", Country = "United States", Phone = "(319) 447-6316", Email = "KidsInc@example.com", Price = 200, DaycareType = DaycareType.LicensedCenter, LicenseNumber = 5, Availability = true }
+                );
 
+            modelBuilder.Entity<DaycareAmenity>()
+                .HasData(
+                new DaycareAmenity { AmenityId = 1, DaycareId = 1 },
+                new DaycareAmenity { AmenityId = 2, DaycareId = 1 },
+                new DaycareAmenity { AmenityId = 3, DaycareId = 2 },
+                new DaycareAmenity { AmenityId = 4, DaycareId = 2 },
+                new DaycareAmenity { AmenityId = 5, DaycareId = 3 },
+                new DaycareAmenity { AmenityId = 6, DaycareId = 3 },
+                new DaycareAmenity { AmenityId = 7, DaycareId = 4 },
+                new DaycareAmenity { AmenityId = 8, DaycareId = 5 },
+                new DaycareAmenity { AmenityId = 5, DaycareId = 5 },
+                new DaycareAmenity { AmenityId = 7, DaycareId = 2 },
+                new DaycareAmenity { AmenityId = 6, DaycareId = 5 }
 
                 );
 
