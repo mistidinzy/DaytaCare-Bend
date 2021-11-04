0336c87823e4b330fba5e69ceb7fa47e2421dfd7
Ian Whitmore <ianwhitmor@gmail.com>
2021-11-04 14:04:37 -0500

    Remove bad unique index add/remove
    Keith says this is ok

--- DaytaCare/Migrations/20211103201312_RemovedUnecessaryNavPropFromAmenity.cs
+++ DaytaCare/Migrations/20211103201312_RemovedUnecessaryNavPropFromAmenity.cs
@@ -6,27 +6,12 @@ namespace DaytaCare.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "IX_DaycareAmenities_AmenityId",
-                table: "DaycareAmenities");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_DaycareAmenities_AmenityId",
-                table: "DaycareAmenities",
-                column: "AmenityId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "IX_DaycareAmenities_AmenityId",
-                table: "DaycareAmenities");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_DaycareAmenities_AmenityId",
-                table: "DaycareAmenities",
-                column: "AmenityId",
-                unique: true);
         }
     }
 }
